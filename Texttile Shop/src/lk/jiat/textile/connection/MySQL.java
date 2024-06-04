290fe68097b5ae22057a8ffe0daf98c7c594abda
ChathuraSudaraka <Chathurabro68@gmail.com>
2024-06-04 21:22:43 +0530

    New Update

---
+++ Texttile Shop/src/lk/jiat/textile/connection/MySQL.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lk.jiat.textile.connection;
+
+/**
+ *
+ * @author Lenovo ideapadGaming
+ */
+public class MySQL {
+    
+}
