658450910d41cb33ece2c5b248767befee4994d2
ChathuraSudaraka <Chathurabro68@gmail.com>
2024-06-04 21:31:43 +0530

    MySQL class

--- Texttile Shop/src/lk/jiat/textile/connection/MySQL.java
+++ Texttile Shop/src/lk/jiat/textile/connection/MySQL.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lk.jiat.textile.connection;
 
-/**
- *
- * @author Lenovo ideapadGaming
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class MySQL {
-    
+
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/adyapana-institute", "root", "Same2u");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ResultSet execute(String query) throws Exception {
+        Statement statement = connection.createStatement();
+        if (query.startsWith("SELECT")) {
+            return statement.executeQuery(query);
+        } else {
+            statement.executeUpdate(query);
+            return null;
+        }
+    }
 }
